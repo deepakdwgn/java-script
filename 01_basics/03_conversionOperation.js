71a5fd944765ad0ccf50b2a02cd013bc45a4a591
Deepak Dewangan <deepakdwgn10@gmail.com>
2025-11-19 13:45:54 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = 45
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let velueInNumber = Number(score)
-console.log(typeof velueInNumber);
+//console.log(typeof velueInNumber);
 
-console.log(velueInNumber);
+//console.log(velueInNumber);
 
 
 // "33" => 33
@@ -18,7 +18,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => True; 0 => False;
@@ -29,5 +29,52 @@ console.log(booleanIsLoggedIn);
 let somenumber = 345
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//***** Operations *****/
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Deepak"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("2" + 3);
+// console.log(2 + "3");
+// console.log("2" + "3");
+// console.log("2" + 3 + 3);
+// console.log(2 + 3 + "3");
+
+// console.log(2 + 3 * 3 % 4);
+// console.log((2 + 3) * 3 % 4);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter; // prefix
+gameCounter++; // profix
+
+
+console.log(gameCounter);
+
+
+
