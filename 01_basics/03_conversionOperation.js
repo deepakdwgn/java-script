d3a295046642b1ea24bd9ec6da092fa564ee30c1
Deepak Dewangan <deepakdwgn10@gmail.com>
2025-11-19 12:15:13 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 45
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let velueInNumber = Number(score)
+console.log(typeof velueInNumber);
+
+console.log(velueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => True; 0 => False;
+// "" => False
+// "Deepak" => True
+
+
+let somenumber = 345
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
