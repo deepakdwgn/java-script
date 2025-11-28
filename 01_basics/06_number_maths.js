e2685c876cc11f539fed7a2e9ce2245d0515c993
Deepak Dewangan <deepakdwgn10@gmail.com>
2025-11-28 13:01:17 +0000

    maths and Number

---
+++ 01_basics/06_number_maths.js
@@ -0,0 +1,40 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const nextnumber = 45.846
+
+// console.log(nextnumber.toPrecision(3));
+
+const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++ Maths +++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-45));
+// console.log(Math.round(6.1));
+// console.log(Math.ceil(6.1));
+// console.log(Math.floor(6.9));
+
+// console.log(Math.max(6, 5, 8, 2));
+// console.log(Math.min(6, 5, 8, 2));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 + 1));
+
+// console.log(Math.sqrt(25));
+
+const min = 20
+const max = 50
+
+console.log(Math.floor(Math.random () * (max - min + 1)) + min);
